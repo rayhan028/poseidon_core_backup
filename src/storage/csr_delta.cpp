76e8e30cbcf78b636a2a03ac0d15bd3f7d3e7920
Muhammad Attahir Jibril <muhammad-attahir.jibril@tu-ilmenau.de>
2021-12-08 11:27:49 +0100

    minor fix in delta store

--- src/storage/csr_delta.cpp
+++ src/storage/csr_delta.cpp
@@ -7,29 +7,63 @@ void csr_delta::initialize() {
 void csr_delta::add_update_delta(uint64_t nid, std::vector<uint64_t> &&ids,
                                  std::vector<double> &&weights) {
   // TODO handle multiple updated states of same nid
-  update_deltas_.store({nid, delta_element::element_type::node_id, nid});
+  delta_element nid_elem;
+  nid_elem.node_id_ = nid;
+  nid_elem.type_ = delta_element::element_type::node_id;
+  nid_elem.val_ = nid;
 
-  for (auto id : ids)
-    update_deltas_.store({nid, delta_element::element_type::neighbour_id, id});
+  update_deltas_.store(std::move(nid_elem));
+
+  for (auto id : ids) {
+    delta_element ngbr_id_elem;
+    ngbr_id_elem.node_id_ = nid;
+    ngbr_id_elem.type_ = delta_element::element_type::neighbour_id;
+    ngbr_id_elem.val_ = id;
+
+    update_deltas_.store(std::move(ngbr_id_elem));
+  }
 
   for (auto w : weights) {
     uint64_t buf;
     std::memcpy(&buf, &w, sizeof(double));
-    update_deltas_.store({nid, delta_element::element_type::rship_weight, buf});
+
+    delta_element rweight_elem;
+    rweight_elem.node_id_ = nid;
+    rweight_elem.type_ = delta_element::element_type::rship_weight;
+    rweight_elem.val_ = buf;
+
+    update_deltas_.store(std::move(rweight_elem));
   }
 }
 
 void csr_delta::add_append_delta(uint64_t nid, std::vector<uint64_t> &&ids,
                                  std::vector<double> &&weights) {
-  append_deltas_.store({nid, delta_element::element_type::node_id, nid});
+  delta_element nid_elem;
+  nid_elem.node_id_ = nid;
+  nid_elem.type_ = delta_element::element_type::node_id;
+  nid_elem.val_ = nid;
 
-  for (auto id : ids)
-    append_deltas_.store({nid, delta_element::element_type::neighbour_id, id});
+  append_deltas_.store(std::move(nid_elem));
+
+  for (auto id : ids) {
+    delta_element ngbr_id_elem;
+    ngbr_id_elem.node_id_ = nid;
+    ngbr_id_elem.type_ = delta_element::element_type::neighbour_id;
+    ngbr_id_elem.val_ = id;
+
+    append_deltas_.store(std::move(ngbr_id_elem));
+  }
 
   for (auto w : weights) {
     uint64_t buf;
     std::memcpy(&buf, &w, sizeof(double));
-    append_deltas_.store({nid, delta_element::element_type::rship_weight, buf});
+
+    delta_element rweight_elem;
+    rweight_elem.node_id_ = nid;
+    rweight_elem.type_ = delta_element::element_type::rship_weight;
+    rweight_elem.val_ = buf;
+
+    append_deltas_.store(std::move(rweight_elem));
   }
 }
 
